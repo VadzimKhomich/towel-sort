ee0e011edebffcb70cb8b675137785c0f3a65a84
vadzim khomich <vadzim.khomich@activeplatform.com>
2024-08-09 16:03:16 +0300

    task towelSort done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort (matrix = []) {
+	let resultArray = []
+
+for (let i = 0; i < matrix.length; i++) {
+    if( i === 0 || i % 2 === 0) {
+       resultArray.push(...matrix[i])
+    } else {
+        resultArray.push(...matrix[i].reverse())
+    }
+}
+return resultArray
 }
